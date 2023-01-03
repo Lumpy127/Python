e34abedd9e5e88f3ee51ce883c7a5f095519f697
Adem Sherifov <adem.sherifov@gmail.com>
2023-01-03 16:17:51 +0200

    Imported calendar and created a loop for the selected weekday. Next step is to count the result.

--- calendar_counter.py
+++ calendar_counter.py
@@ -1,2 +1,10 @@
+import calendar
+
 weekday = input("Specify a weekday:\n1.Monday\n2.Tuesday\n3.Wednesday\n4.Thursday\n5.Friday\6.Saturday\n7.Sunday")
 year = input("Provide a specific year: ")
+
+cal = calendar.monthcalendar(2022, 1)
+for i in cal:
+    realweekday = int(weekday) - 1
+    if i[int(realweekday)] != 0:
+        print("Weekday count")
